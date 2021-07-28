97078df69777c0416f4ad00dd2f3fd92fc9f7859
Joseph Tharpe <solowun6@aol.com>
2021-07-27 23:21:08 -0700

    mongoose pt 3

--- index.js
+++ index.js
@@ -4,25 +4,41 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 connect.then(() => {
-    console.log(`connected correctly to server`);
 
-    const newCampsite = new Campsite({
+    console.log('Connected correctly to server');
+
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
-    });
-
-    newCampsite.save()
+    })
         .then(campsite => {
             console.log(campsite);
-            return Campsite.find()
+
+            return Campsite.findByIdAndUpdate(campsite._id, {
+                $set: { description: 'Updated Test Document' }
+            }, {
+                new: true
+            });
         })
-        .then(campsites => {
-            console.log(campsites);
+        .then(campsite => {
+            console.log(campsite);
+
+            campsite.comments.push({
+                rating: 5,
+                text: 'What a magnificent view!',
+                author: 'Tinus Lorvaldes'
+            });
+
+            return campsite.save();
+        })
+        .then(campsite => {
+            console.log(campsite);
             return Campsite.deleteMany();
         })
         .then(() => {
@@ -32,5 +48,4 @@ connect.then(() => {
             console.log(err);
             mongoose.connection.close();
         });
-
 });
